9c4da9d5e6046abe67106ba773a8baedd5b44806
AJ Harmond <ajharmond@gmail.com>
2019-03-11 15:23:40 -0600

    add items

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import List from './List';
+import GroceryForm from './GroceryForm';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  state = { groceries: [] };
+
+  addItem = (name) => {
+    const { groceries } = this.state;
+    const grocery = { name, id: this.getUniqueId(), complete: false }
+    this.setState({ groceries: [...groceries, grocery] });
+  }
+
+  getUniqueId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
   render() {
+    const { groceries } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GroceryForm addItem={this.addItem} /> 
+        <List name="Grocery List" items={groceries} />
       </div>
-    );
+      );
   }
 }
 
